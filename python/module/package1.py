f56342892fa560f3db2749b0c59a4bd98c0b9526
Akiyo <carlylezqy@qq.com>
2023-04-18 19:43:06 +0900

    Update package1.py

--- python/module/package1.py
+++ python/module/package1.py
@@ -2,6 +2,10 @@ def package1():
     a = 1
     assert a == 1
     print("Aplpe, pe, dsf, sadw")
+    aplpe = 1
+    dasf = 123
+    asd2132 = '1'
+    asd80 = "12"
 
 if __name__ == "__main__":
     package1()
