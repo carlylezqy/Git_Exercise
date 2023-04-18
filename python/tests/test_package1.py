a332ee24e1d3854268e39b802097e90c43020090
carlylezqy <carlylezqy@qq.com>
2023-04-18 18:09:23 +0900

    modified

--- python/tests/test_package1.py
+++ python/tests/test_package1.py
@@ -1,9 +1,10 @@
 import os 
 import sys
-if __name__ == "__main__":
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(current_path + '/..')
-    import module
-    
-    print("进行中文测试")
-    module.package1.package1()
+class TestModule:
+    def test_package1(self):
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        sys.path.append(current_path + '/..')
+        import module
+        
+        print("进行中文测试")
+        module.package1.package1()
