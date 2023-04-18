0fb4e112bb9476c52fc3bcfc0581a946067d7161
carlylezqy <carlylezqy@qq.com>
2023-04-18 17:57:15 +0900

    1

--- python/test/base_test.py
+++ python/test/base_test.py
@@ -1,10 +1,9 @@
 import os 
 import sys
-import module
-
-
 if __name__ == "__main__":
     current_path = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(current_path + '/..')
+    import module
+    
     print("进行中文测试")
     module.package1.package1()
