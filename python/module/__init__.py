2681e3a0d13cf965b67d8934fd8e9f0c65f1ab81
carlylezqy <carlylezqy@qq.com>
2023-04-18 17:55:30 +0900

    change formart

--- python/module/__init__.py
+++ python/module/__init__.py
@@ -1,1 +1,2 @@
+# ruff: noqa: F401
 from . import package1
--- python/test/base_test.py
+++ python/test/base_test.py
@@ -1,7 +1,10 @@
-import os; current_path = os.path.dirname(os.path.abspath(__file__))
-import sys; sys.path.append(current_path + '/..')
-
+import os 
+import sys
 import module
+
+
 if __name__ == "__main__":
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(current_path + '/..')
     print("进行中文测试")
     module.package1.package1()
