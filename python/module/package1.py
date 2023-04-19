4a0129e68f1f2aadb985f99c5947750326e660e8
Akiyo <carlylezqy@qq.com>
2023-04-19 10:01:09 +0900

    Update package1.py

--- python/module/package1.py
+++ python/module/package1.py
@@ -6,6 +6,9 @@ def package1():
     dasf = 123
     asd2132 = '1'
     asd80 = "12"
+    # aplpe, dasf, asd2132, asd80
+    print(aplpe, dasf, asd2132, asd80)
+    
 
 if __name__ == "__main__":
     package1()
